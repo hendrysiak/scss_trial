f0f1ecd488c7051d455d7fdf2d8785a5271eed5c
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2020-03-14 22:38:35 +0100

    Ended styles for header and navigation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./scss/index.scss";
+
+document.querySelector('.nav__hamburger').addEventListener('click', function(){
+  this.classList.toggle('--active');
+  this.parentNode.classList.toggle('nav--active')
+})
